4713c0ded55eab8af3bcdcbfecc4280377485e02
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-10-02 08:42:03 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,25 @@
 #include <Arduino.h>
 #include <LittleFS.h>
 
-#include "v010/SC10_WindScape.h"
+#include "v010/SC10_WindScape_002.h"
 
 WindScapeSimulator g_SC10;
 
 void setup() {
+  Serial.begin(115200);
+  delay(100);
+ 
   // (선택) 로그 레벨
-  // SC10_Logger::setLevel(SC10_LOG_DEBUG);
+  SC10_Logger::setLevel(SC10_LOG_DEBUG);
 
+  /*
+  if (!LittleFS.begin(true)) {
+      SC10_Logger::log(SC10_LOG_ERROR,"LittleFS mount failed");
+  } else {
+      SC10_Logger::log(SC10_LOG_INFO,"LittleFS mounted");
+  }
+  */
+  
   g_SC10.SC10_init();
 
   Serial.println("[BOOT] Ready. Try /api/state, /api/scan, /api/config, /api/diag, /api/logs...");
