a905c2375b315d97a6260875ea4cc22a29eaf938
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-10-02 09:34:06 +0900

    Update SC10_Const_002.h

--- src/v010/SC10_Const_002.h
+++ src/v010/SC10_Const_002.h
@@ -129,7 +129,7 @@ struct SC10_StaCredential {
 struct WindConfig {
     // --- Wi-Fi 설정 ---
     int wifi_mode = G_SC10_WIFI_MODE_STA;  ///< 현재 Wi-Fi 모드
-    SC10_StaCredential sta_networks[SC10_Const.MAX_STA_NETWORKS]; ///< STA 네트워크 목록
+    SC10_StaCredential sta_networks[SC10_Const::MAX_STA_NETWORKS]; ///< STA 네트워크 목록
     int sta_network_count = 0;             ///< 실제 저장된 STA 네트워크 수
     char ap_ssid[32] = "SC10_Config_AP";   ///< AP 모드 SSID
     char ap_password[64] = "newpassword";  ///< AP 모드 Password
