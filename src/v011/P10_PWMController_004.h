110377d08cc7e1a71313be3a7421a652ee2f7216
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-10-15 19:43:39 +0900

    Update P10_PWMController_004.h

--- src/v011/P10_PWMController_004.h
+++ src/v011/P10_PWMController_004.h
@@ -4,22 +4,64 @@
 #include <Arduino.h>
 #include "D10_Logger_004.h"
 
-class CL_P10_PWMController {
+#include <ArduinoJson.h>
+
+class CL_P10_PWM {
 public:
-    static void init(int pin, int channel, int freq, int res) {
-        ledcSetup(channel, freq, res);
-        ledcAttachPin(pin, channel);
-        CL_F10_Logger::log(D10_LOG_INFO, "PWM initialized");
+    CL_P10_PWM() = default;
+
+    void init(uint8_t pin, uint8_t ch, uint32_t freq, uint8_t res) {
+        pwmPin = pin;
+        pwmChannel = ch;
+        pwmFreq = freq;
+        pwmRes = res;
+        setup();
     }
 
-    static void setDuty(int channel, float percent, int resolutionBits) {
+    void init() {
+        ledcSetup(pwmChannel, pwmFreq, pwmRes);
+        ledcAttachPin(pwmPin, pwmChannel);
+    }
+
+    void set_pwmPin(uint8_t newPin) {
+        if (newPin == pwmPin) return;
+        ledcDetachPin(pwmPin);
+        pwmPin = newPin;
+        ledcAttachPin(pwmPin, pwmChannel);
+    }
+
+    void set_pwmChannel(uint8_t newCh) {
+        pwmChannel = newCh;
+        setup();
+    }
+
+    void set_pwmFrequency(uint32_t freq) {
+        pwmFreq = freq;
+        setup();
+    }
+
+    void set_pwmResolution(uint8_t bits) {
+        pwmRes = bits;
+        setup();
+    }
+
+    void set_pwmDuty(float percent) {
         percent = constrain(percent, 0.0f, 100.0f);
-        uint32_t maxDuty = (1 << resolutionBits) - 1;
-        uint32_t duty = (uint32_t)(percent / 100.0f * maxDuty);
-        ledcWrite(channel, duty);
+        uint32_t duty = (uint32_t)((pow(2, pwmRes) - 1) * (percent / 100.0f));
+        ledcWrite(pwmChannel, duty);
     }
 
-    static void stop(int channel) {
-        ledcWrite(channel, 0);
+    void toJson(JsonDocument& doc) {
+        JsonObject pwm = doc["pwm"].to<JsonObject>();
+        pwm["pin"] = pwmPin;
+        pwm["ch"] = pwmChannel;
+        pwm["freq"] = pwmFreq;
+        pwm["res"] = pwmRes;
     }
+
+private:
+    uint8_t pwmPin = 6;
+    uint8_t pwmChannel = 0;
+    uint32_t pwmFreq = 25000;
+    uint8_t pwmRes = 10;
 };
