0ffa0d30e34255c9f1b0acec2507150a7f5d84eb
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-10-17 09:05:43 +0900

    Update WiFi and Simulation includes to version 006

--- src/v011/W10_WebAPI_006.h
+++ src/v011/W10_WebAPI_006.h
@@ -23,8 +23,8 @@
 #include "A10_Const_006.h"
 #include "C10_ConfigManager_006.h"
 #include "D10_Logger_004.h"
-#include "M10_WiFiManager_004.h"
-#include "S10_Simulation_004.h"
+#include "M10_WiFiManager_006.h"
+#include "S10_Simulation_006.h"
 #include "P10_PWM_ctrl_005.h"
 
 class CL_W10_WebAPI {
