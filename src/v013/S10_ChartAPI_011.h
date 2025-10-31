244ca03ec400b9798f67d1036a5ecd0c75c2b288
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-10-31 20:11:27 +0900

    Update S10_ChartAPI_011.h

--- src/v013/S10_ChartAPI_011.h
+++ src/v013/S10_ChartAPI_011.h
@@ -39,7 +39,7 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <deque>
-#include "A10_Const_011.h"
+#include "A10_Const_012.h"
 #include "S10_Simulation_011.h" // s_chartBuffer / ST_S10_ChartEntry 사용
 
 class CL_S10_ChartAPI {
