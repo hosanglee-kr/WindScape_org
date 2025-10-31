c9e11a263bc81b0c1cece6470812f0ca01e08ac0
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-10-31 23:37:43 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,9 @@
 
 #include <Arduino.h>
 #include <LittleFS.h>
-#include "v012/WS10_Main_010.h"
+
+#include "v01ㄷ/WS10_Main_012.h"
+// #include "v012/WS10_Main_010.h"
 
 // 메인 컨트롤러 인스턴스
 CL_WS10_WindScapeSimulator g_WS10;
