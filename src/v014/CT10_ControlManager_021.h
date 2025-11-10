d0e70cbc04fb5b10eeabcaf7f99a59ef0b8005fe
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-11-10 23:13:21 +0900

    Update CT10_ControlManager_021.h

--- src/v014/CT10_ControlManager_021.h
+++ src/v014/CT10_ControlManager_021.h
@@ -49,8 +49,8 @@
 #include <time.h>
 #include <string.h>
 
-#include "A10_Const_014.h"
-#include "C10_ConfigManager_021.h"
+#include "A10_Const_015.h"
+#include "C10_ConfigManager_022.h"
 #include "S10_Simulation_018.h"
 #include "P10_PWM_ctrl_014.h"
 #include "D10_Logger_016.h"
