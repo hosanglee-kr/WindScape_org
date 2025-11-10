86deb58a8f41c97d494c66b9a67b736abf843273
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-11-10 23:14:47 +0900

    Update N10_NvsManager_017.h

--- src/v014/N10_NvsManager_017.h
+++ src/v014/N10_NvsManager_017.h
@@ -46,8 +46,8 @@
 #include <Preferences.h>
 #include <string.h>
 
-#include "A10_Const_014.h"
-#include "C10_ConfigManager_021.h"
+#include "A10_Const_015.h"
+#include "C10_ConfigManager_022.h"
 #include "D10_Logger_016.h"
 
 // ------------------------------------------------------
