484a047fe5dbcffe48985601c39aedac0e77c718
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-11-11 19:36:43 +0900

    Update S10_Simulation_018.h

--- src/v014/S10_Simulation_018.h
+++ src/v014/S10_Simulation_018.h
@@ -57,8 +57,10 @@
 #include "D10_Logger_016.h"
 #include "P10_PWM_ctrl_014.h"
 
+
+#inclide "W10_WebAPI_IF_020.h"
 // ✅ 전방 선언으로 순환참조 방지
-class CL_W10_WebAPI;
+// class CL_W10_WebAPI;
 
 // ======================================================
 // CL_S10_Simulation
