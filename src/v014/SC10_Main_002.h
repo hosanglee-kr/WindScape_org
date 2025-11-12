f7e0a772f6c8c0ec55afa3aad4537cc18c0d7065
hosanglee-kr <ddongl@gmail.com>
2025-11-12 09:22:44 +0900

    modified:   src/v014/

--- src/v014/SC10_Main_002.h
+++ src/v014/SC10_Main_002.h
@@ -31,7 +31,7 @@
 #include "P10_PWM_ctrl_014.h"
 #include "S10_Simulation_018.h"
 #include "S20_WindSolver_021.h"
-#include "W10_WebAPI_022.h"
+#include "W10_Web_023.h"
 #include "WF10_WiFiManager_023.h"
 
 
