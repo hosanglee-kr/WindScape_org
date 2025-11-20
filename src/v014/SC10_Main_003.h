c202360705925927cf07992b581f0143dd365a42
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-11-21 08:56:22 +0900

    Update CT10_ControlManager include version

--- src/v014/SC10_Main_003.h
+++ src/v014/SC10_Main_003.h
@@ -25,7 +25,7 @@
 
 #include "A10_Const_015.h"
 #include "C10_ConfigManager_023.h"
-#include "CT10_ControlManager_021.h"
+#include "CT10_ControlManager_022.h"
 #include "D10_Logger_016.h"
 #include "M10_MotionLogic_016.h"
 #include "N10_NvsManager_018.h"
