e1d477eb827654f295bddc3ebe55449c7b8ebb37
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-11-21 19:36:57 +0900

    Update ControlManager include to version 022

--- src/v014/W10_Web_Routes_025.cpp
+++ src/v014/W10_Web_Routes_025.cpp
@@ -33,7 +33,7 @@
 
 #include "W10_Web_025.h"
 #include "M10_MotionLogic_016.h"
-#include "CT10_ControlManager_021.h"
+#include "CT10_ControlManager_022.h"
 #include "WF10_WiFiManager_023.h" // CL_WF10_WiFiManager::scanNetworksToJson 사용을 위해 포함
 
 
