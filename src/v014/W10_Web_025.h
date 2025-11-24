0b9cad52681c60767e3c39571a3f61dbd29b246b
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-11-24 21:17:21 +0900

    Update W10_Web_025.h

--- src/v014/W10_Web_025.h
+++ src/v014/W10_Web_025.h
@@ -116,7 +116,7 @@ private:
 	// --------------------------------------------------
 	// 유틸리티 함수
 	// --------------------------------------------------
-    void _broadcast(AsyncWebSocket* p_ws, JsonDocument& p_doc, bool p_diffOnly) {
+    void _broadcast(AsyncWebSocket* p_ws, JsonDocument& p_doc, bool p_diffOnly);
 	
 	
 	// v012의 _applyHeaders 로직 복구 및 send 함수에 적용
