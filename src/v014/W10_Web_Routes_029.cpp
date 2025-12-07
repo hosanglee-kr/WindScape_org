3a0af36416ce7fb83d1414f4ce36a3fd04dd764b
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-12-07 11:11:36 +0900

    Update W10_Web_Routes_029.cpp

--- src/v014/W10_Web_Routes_029.cpp
+++ src/v014/W10_Web_Routes_029.cpp
@@ -33,7 +33,7 @@
 
 #include "W10_Web_030.h"
 #include "M10_MotionLogic_016.h"
-#include "CT10_ControlManager_024.h"
+#include "CT10_ControlManager_025.h"
 #include "WF10_WiFiManager_025.h"
 
 // ------------------------------------------------------
