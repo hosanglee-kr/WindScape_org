32d74bb18337611fd12a4e6405abc0449e236f78
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-12-07 11:10:31 +0900

    Update SC10_Main_005.h

--- src/v014/SC10_Main_005.h
+++ src/v014/SC10_Main_005.h
@@ -23,9 +23,9 @@
 #include <WiFiMulti.h>
 #include <esp_task_wdt.h>
 
-#include "A10_Const_015.h"
+#include "A10_Const_016.h"
 #include "C10_Config_029.h"
-#include "CT10_ControlManager_024.h"
+#include "CT10_ControlManager_025.h"
 #include "D10_Logger_016.h"
 #include "M10_MotionLogic_016.h"
 #include "N10_NvsManager_019.h"
