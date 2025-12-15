149fdd49d1de0dfebfb98f78f37a36333b76c158
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2025-12-15 09:19:27 +0900

    Update SC10_Main_005.h

--- src/v014/SC10_Main_005.h
+++ src/v014/SC10_Main_005.h
@@ -30,7 +30,7 @@
 #include "M10_MotionLogic_016.h"
 #include "N10_NvsManager_019.h"
 #include "P10_PWM_ctrl_014.h"
-#include "S10_Simulation_020.h"
+#include "S10_Simul_021.h"
 #include "S20_WindSolver_021.h"
 #include "W10_Web_030.h"
 #include "WF10_WiFiManager_025.h"
